ecf99e9ee3c14c4aa01ecd10db52beb2e7f3cfec
João Pedro <joaopjt@icloud.com>
2023-09-10 16:48:06 -0300

    added c() method values

--- src/index.js
+++ src/index.js
@@ -180,7 +180,7 @@ class FireExtinguisher {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['water'],
       web: []
     };
     
@@ -196,19 +196,19 @@ class FireExtinguisher {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['predict'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['hand'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['heart'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['gold'];
           break;
       }
     }
